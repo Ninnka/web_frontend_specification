e2acd7e183d96a188fef621f2cfa266ae33a2bde
rennzh <465543032@qq.com>
2016-10-16 23:57:59 +0800

    first commit and add file to repository

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            "tab",
+            {"SwitchCase": 1}
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [1, {"vars": "all", "args": "after-used"}],
+        "no-console": 0
+    }
+};
